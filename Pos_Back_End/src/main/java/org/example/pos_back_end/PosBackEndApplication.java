0872bb1ee62687ad1052b15c148b225aa50b177d
Manujaravishka <manujaravishka2004@gmail.com>
2026-02-21 21:34:18 +0530

    first commit

---
+++ Pos_Back_End/src/main/java/org/example/pos_back_end/PosBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.pos_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosBackEndApplication.class, args);
+    }
+
+}
---
+++ Pos_Back_End/src/test/java/org/example/pos_back_end/PosBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pos_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
