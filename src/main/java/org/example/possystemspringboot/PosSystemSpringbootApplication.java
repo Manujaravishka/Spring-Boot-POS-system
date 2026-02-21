794e2c53c8886a2211ad2da8cacb951d4a5c09a0
Manujaravishka <manujaravishka2004@gmail.com>
2026-02-21 21:32:38 +0530

    first commit

---
+++ src/main/java/org/example/possystemspringboot/PosSystemSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.possystemspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSystemSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosSystemSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/possystemspringboot/PosSystemSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.possystemspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
